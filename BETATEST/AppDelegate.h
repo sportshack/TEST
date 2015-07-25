bcd0148ddd44e776dafbc846b3fecc8deae3275d
Felipe Marques <feliperfmarques@gmail.com>
2015-07-24 23:31:49 -0300

    Initial Commit

---
+++ BETATEST/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BETATEST
+//
+//  Created by Felipe Marques on 24/07/15.
+//  Copyright (c) 2015 Felipe Marques. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BETATEST/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BETATEST
+//
+//  Created by Felipe Marques on 24/07/15.
+//  Copyright (c) 2015 Felipe Marques. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
